f57e2ff831fa0775aea40dce28e4c2ea6c0abc6b
Matheus Pedrosa <math.pedrosa@hotmail.com>
2021-05-24 21:35:40 -0300

    Reachability implementation to check connection

--- got data yet/ViewController.swift
+++ got data yet/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Reachability
 
 class ViewController: BaseViewController {
 
+    let reachability = try! Reachability()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        checkConnection()
+    }
+    
+    private func checkConnection() {
+        switch reachability.connection {
+        case .cellular:
+            print("celullar")
+        case .wifi:
+            print("wifi")
+        default:
+            print("unavailable")
+        }
     }
 
 }
